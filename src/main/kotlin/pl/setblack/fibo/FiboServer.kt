82ee27655e3f71936e1cdfed915e6cf260403c9e
yaskier <r.ruiz.giner@gmail.com>
2018-06-13 12:59:16 +0200

    fibonacci via http recursion

--- src/main/kotlin/pl/setblack/fibo/FiboServer.kt
+++ src/main/kotlin/pl/setblack/fibo/FiboServer.kt
@@ -10,7 +10,7 @@ import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.router
 import reactor.core.publisher.Mono
 import reactor.ipc.netty.http.server.HttpServer
-import java.util.function.Function
+import java.time.Duration
 
 class FiboServer {
     fun start() {
@@ -18,13 +18,37 @@ class FiboServer {
             GET("/fib/{n}")
             { request ->
                 val n = Integer.parseInt(request.pathVariable("n"))
-                    /*val n_1 = WebClient.create("http://localhost:8080").get().uri("/fib/{n}", n - 1)
-                            .accept(MediaType.TEXT_HTML).exchange()
-                            .flatMap { resp -> resp.bodyToMono(String::class.java) }
-                            .map<Int>{ Integer.parseInt(it) }
-                    */
-                    //ServerResponse.ok().contentType(MediaType.TEXT_HTML).body(fromPublisher<String, Mono<String>>(result, String::class.java))
-                ServerResponse.ok().contentType(MediaType.TEXT_HTML).body(fromObject("Hello world!"))
+                println("number $n")
+
+                if (n < 2) {
+                    ServerResponse.ok().contentType(MediaType.TEXT_HTML).body(fromObject("1"))
+                } else {
+
+                    val n_1 = WebClient.create("http://localhost:8080")
+                            .get()
+                            .uri("/fib/{n}", n - 1)
+                            .accept(MediaType.TEXT_HTML)
+                            .exchange()
+                            .flatMap { it.bodyToMono(String::class.java) }
+                            .map<Int>( Integer::parseInt )
+
+                    val n_2 = WebClient.create("http://localhost:8080")
+                            .get()
+                            .uri("/fib/{n}", n - 2)
+                            .accept(MediaType.TEXT_HTML)
+                            .exchange()
+                            .flatMap { it.bodyToMono(String::class.java) }
+                            .map<Int>( Integer::parseInt )
+
+                    val result = n_1.flatMap { n1 -> n_2.map { it+n1 } }
+                            .map { it.toString() }
+
+
+                    println("result: $n")
+
+                    ServerResponse.ok().contentType(MediaType.TEXT_HTML).body(fromPublisher<String, Mono<String>>(result, String::class.java))
+                    //ServerResponse.ok().contentType(MediaType.TEXT_HTML).body(fromObject("my fibbonacci for $n is $result"))
+                }
             }
         }
 
